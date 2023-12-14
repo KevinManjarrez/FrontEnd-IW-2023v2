31b0320ed1cb4f8551639251cc936825ea8f8759
KevinManjarrez <72154613+KevinManjarrez@users.noreply.github.com>
2023-12-14 16:11:41 -0700

    Cambio de archivo DetalleFTable

--- src/ecommerce/orders/components/tables/OrdenesDetalleFTable.jsx
+++ src/ecommerce/orders/components/tables/OrdenesDetalleFTable.jsx
@@ -1,50 +1,33 @@
-//FIC: React
-import React, { useEffect, useMemo, useState } from "react";
-//FIC: Material UI
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  IconButton,
+  Tooltip,
+  Dialog
+} from "@mui/material";
 import { MaterialReactTable } from "material-react-table";
-import { Box, Stack, Tooltip, Button, IconButton, Dialog } from "@mui/material";
-import { MRT_Localization_ES } from "material-react-table/locales/es";
-import BarActionsTable from "../../../../share/components/elements/bars/BarActionsTable";
+//import InfoIcon from "@mui/icons-material/Info";
+//import { Componente } from "@mui/material"; // Sustituye "Componente" por el nombre del componente que desees importar
 
-import { GetOneOrderByID } from "../../service/remote/get/GetOneOrderByID";
+import OrdenesDetalleFModal from "../modals/OrdenesDetalleFModal"; 
+import { OrdenesDetalleFModel } from "../../models/OrdenesDetalleFModel";
 
-//FIC: Modals
-import OrdenesDetalleFModal from "../modals/OrdenesDetalleFModal";
-//REDUX
 import { useSelector } from "react-redux";
 
-const OrdenesDetallesFColumn = [
-  {
-    accessorKey: "IdTipoEstatusOK",
-    header: "Id Tipo Estatus OK",
-    size: 30, //small column
-  },
-  {
-    accessorKey: "Actual",
-    header: "Actual",
-    size: 30, //small column
-  },
-  {
-    accessorKey: "Observacion",
-    header: "Observacion",
-    size: 150, //small column
-  },
-];
-
-const OrdenesDetalleFTable = ({ }) => {
+const OrdenesDetalleFTable = ({
+  
+}) => {
   const [loadingTable, setLoadingTable] = useState(true);
-  const [OrdenesDetalleFData, setOrdenesDetalleFData] = useState([]);
+  const [OrdenesDetalleData, setOrdenesDetalleData] = useState([]);
   const [pedidosDetalleEstatusF, setPedidosDetalleEstatusF] = useState([]);
-  const [OrdenesDetalleFShowModal, setOrdenesDetalleFShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
     //Con redux sacar la data que se envió del otro archivo (ShippingsTable)
-  const [selectedRowIndex, setSelectedRowIndex] = useState(null); //Para saber cual es la fila y pasarla para el color de la tabla
-
-  const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesData);
+    const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesDetalleData);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        setOrdenesDetalleFData(selectedOrdenesData.ordenes_detalle.pedidos_detalle_ps_estatus_f); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
+        setOrdenesDetalleData(selectedOrdenesData.pedidos_detalle_ps_estatus_f); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
         setLoadingTable(false);
       } catch (error) {
         console.error("Error al obtener datos:", error);
@@ -53,63 +36,43 @@ const OrdenesDetalleFTable = ({ }) => {
     fetchData();
   }, []);
 
-  const handleReload = async () => {
-    const OneOrdenesData = await GetOneOrderByID(selectedOrdenesData.IdInstitutoOK,selectedOrdenesData.IdNegocioOK,selectedOrdenesData.IdOrdenOK);
-    setOrdenesEstatusData(OneOrdenesData.ordenes_detalle.pedidos_detalle_ps_estatus_f);
-    setSelectedRowIndex(null);
-  };
+  const OrdenesDetalleColumn = [
+    {
+        accessorKey: "IdTipoEstatusOK",
+        header: "Id Tipo Estatus OK",
+        size: 30, //small column
+      },
+      {
+        accessorKey: "Actual",
+        header: "Actual",
+        size: 30, //small column
+      },
+      {
+        accessorKey: "Observacion",
+        header: "Observacion",
+        size: 150, //small column
+      },
+  ];
 
- 
   return (
     <Box>
-      <Box>
-        <MaterialReactTable
-          columns={OrdenesDetallesFColumn}
-          data={OrdenesDetalleFData}
-          state={{isLoading: loadingTable}}
-          initialState={{ density: "compact", showGlobalFilter: true }}
-          enableColumnActions={false}
-          localization={MRT_Localization_ES}
-          enableStickyHeader
-          muiTableContainerProps={{
-            sx: {
-              "&::-webkit-scrollbar": { display: "none" },
-              msOverflowStyle: "none",
-              scrollbarWidth: "none",
-              overflow: "auto",
-              width: "parent",
-            },
-          }}
-          positionToolbarAlertBanner="bottom"
-          renderTopToolbarCustomActions={({ table }) => (
-              <BarActionsTable
-            handleBtnAdd={() => setOrdenesDetalleFShowModal(true)}
-            handleBtnDetails={() => console.log("clic handleBtnDetails")}
-            handleBtnReload={() => handleReload()}
-          />
-          )}
-          muiTableBodyRowProps={({ row }) => ({
-            onClick: () => {
-              setSelectedRowIndex(row.original);
-              setSelectedRowIndex(row.id);
-            },
-          })}
+      <MaterialReactTable
+        columns={OrdenesDetalleColumn}
+        data={OrdenesDetalleData}
+        state={{ isLoading: loadingTable }}
+        // Otras props de tu tabla...
       />
-      </Box>
 
-      {/* M O D A L E S */}   
-      <Dialog open={OrdenesDetalleFShowModal}>
+      {/* Modal para la vista detallada */}
+      <Dialog open={showModal} >
         <OrdenesDetalleFModal
-          OrdenesDetalleFShowModal={OrdenesDetalleFShowModal}
-          setOrdenesDetalleFShowModal={setOrdenesDetalleFShowModal}
-          handleReload={handleReload}
-          row={selectedOrdenesData} //Pasar como prop los datos que sacamos de redux desde ordentable para 
-          onClose={() => setOrdenesDetalleFShowModal(false)}   //usarlos en InfoAdModal y consecuentemente en formik.
+          open={showModal}
+          // ...otros props necesarios
         />
       </Dialog>
-
     </Box>
   );
 };
+
 export default OrdenesDetalleFTable;
 
