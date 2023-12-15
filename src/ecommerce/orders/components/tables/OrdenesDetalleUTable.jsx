2af457a8d7f9a2d4db10f6fb3451bade4aeae98d
AlexisMJ2 <almorenoja@ittepic.edu.mx>
2023-12-14 22:26:09 -0700

    Se agregaron modals a estatus ubicacion y proceso

--- src/ecommerce/orders/components/tables/OrdenesDetalleUTable.jsx
+++ src/ecommerce/orders/components/tables/OrdenesDetalleUTable.jsx
@@ -5,29 +5,34 @@ import {
   Tooltip,
   Dialog
 } from "@mui/material";
+import { MRT_Localization_ES } from "material-react-table/locales/es";
 import { MaterialReactTable } from "material-react-table";
+
 //import InfoIcon from "@mui/icons-material/Info";
 //import { Componente } from "@mui/material"; // Sustituye "Componente" por el nombre del componente que desees importar
 
-import OrdenesDetalleFModal from "../modals/OrdenesDetalleFModal"; 
-import { OrdenesDetalleFModel } from "../../models/OrdenesDetalleFModel";
+import OrdenesDetalleUModal from "../modals/OrdenesDetalleUModal";
+
+import BarActionsTable from "../../../../share/components/elements/bars/BarActionsTable";
+
 
 import { useSelector } from "react-redux";
 
 const OrdenesDetalleUTable = ({
-  
+
 }) => {
   const [loadingTable, setLoadingTable] = useState(true);
-  const [OrdenesDetalleData, setOrdenesDetalleData] = useState([]);
-  const [pedidosDetalleEstatusU, setPedidosDetalleEstatusU] = useState([]);
+  const [OrdenesDetalleUData, setOrdenesDetalleUData] = useState([]);
+  const [selectedRowIndex, setSelectedRowIndex] = useState(null); //Para saber cual es la fila y pasarla para el color de la tabla
+
   const [showModal, setShowModal] = useState(false);
     //Con redux sacar la data que se envió del otro archivo (ShippingsTable)
-    const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesDetalleData);
+  const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesDetalleData);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        setOrdenesDetalleData(selectedOrdenesData.pedidos_detalle_ps_estatus_u); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
+        setOrdenesDetalleUData(selectedOrdenesData.pedidos_detalle_ps_estatus_u); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
         setLoadingTable(false);
       } catch (error) {
         console.error("Error al obtener datos:", error);
@@ -36,7 +41,13 @@ const OrdenesDetalleUTable = ({
     fetchData();
   }, []);
 
-  const OrdenesDetalleColumn = [
+  const handleReload = async () => {
+    const OneOrdenesData = await GetOneOrderByID(selectedOrdenesData.IdInstitutoOK,selectedOrdenesData.IdNegocioOK,selectedOrdenesData.IdOrdenOK);
+    //setOrdenesEstatusData(OneOrdenesData.ordenes_estatus);
+    setSelectedRowIndex(null);
+  };
+
+  const OrdenesDetalleUColumn = [
     {
         accessorKey: "IdTipoEstatusOK",
         header: "Id Tipo Estatus OK",
@@ -56,17 +67,49 @@ const OrdenesDetalleUTable = ({
 
   return (
     <Box>
+      <Box>
       <MaterialReactTable
-        columns={OrdenesDetalleColumn}
-        data={OrdenesDetalleData}
+        columns={OrdenesDetalleUColumn}
+        data={OrdenesDetalleUData}
         state={{ isLoading: loadingTable }}
-        // Otras props de tu tabla...
-      />
+        initialState={{ density: "compact", showGlobalFilter: true }}
+        enableColumnActions={false}
+        localization={MRT_Localization_ES}
+        enableStickyHeader
+        muiTableContainerProps={{
+          sx: {
+            "&::-webkit-scrollbar": { display: "none" },
+            msOverflowStyle: "none",
+            scrollbarWidth: "none",
+            overflow: "auto",
+            width: "parent",
+          },
+        }}
+        positionToolbarAlertBanner="bottom"
+        renderTopToolbarCustomActions={({ table }) => (
+            <BarActionsTable
+          handleBtnAdd={() => setShowModal(true)}
+          handleBtnDetails={() => console.log("clic handleBtnDetails")}
+          handleBtnReload={() => handleReload()}
+        />
+        )}
+        muiTableBodyRowProps={({ row }) => ({
+          onClick: () => {
+            setSelectedRowIndex(row.original);
+            setSelectedRowIndex(row.id);
+          },
+        })}
+    />
+    </Box>
 
       {/* Modal para la vista detallada */}
-      <Dialog open={showModal} >
-        <OrdenesDetalleFModal
-          open={showModal}
+      <Dialog open={showModal}>
+        <OrdenesDetalleUModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          //handleReload={handleReload}
+          onClose={() => setShowModal(false)}
+
           // ...otros props necesarios
         />
       </Dialog>
