83564f77d4376a516e49ed742e0121f75998cbb1
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2023-12-15 21:02:41 -0700

    Agregacion German

--- src/ecommerce/orders/helpers/OrdenesDetallesFValues.jsx
+++ src/ecommerce/orders/helpers/OrdenesDetallesFValues.jsx
@@ -1,9 +1,9 @@
-import { OrdenesDetallesFModel } from "../models/OrdenesDetallesFModel";
+import { OrdenesDetalleFModel } from "../models/OrdenesDetalleFModel";
 
 //FIC: obtiene los valores capturados en la ventana modal
 //enviados desde el evento onSubmit de Formik
 export const OrdenesDetallesFValues = (values, ordenesDetallesF) => {
-  let OrdenesDetallesF = ordenesDetallesF || OrdenesDetallesFModel();
+  let OrdenesDetallesF = ordenesDetallesF || OrdenesDetalleFModel();
   // Crear un nuevo objeto de estatus
   let nuevoDetalleF = {
     IdTipoEstatusOK: values.IdTipoEstatusOK,
