6d0b77edfdf4c92911c402ca21d9f11c23a79c46
KevinManjarrez <72154613+KevinManjarrez@users.noreply.github.com>
2023-12-15 18:59:58 -0700

    Se terminó el detalle f modal y el detalle F table

--- src/ecommerce/orders/components/modals/OrdenesDetalleFModal.jsx
+++ src/ecommerce/orders/components/modals/OrdenesDetalleFModal.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState,useEffect } from "react";
 import {
   Dialog,
   DialogContent,
@@ -7,48 +8,88 @@ import {
   TextField,
   DialogActions,
   Box,
-  Alert
+  Alert,
+  FormControlLabel,
+  Checkbox,
+  InputLabel,
+  Select,
+  MenuItem 
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import CloseIcon from "@mui/icons-material/Close";
 import SaveIcon from "@mui/icons-material/Save";
 import * as Yup from "yup";
 import { useFormik } from "formik";
-// ... otras importaciones necesarias
-//import { Componente } from "@mui/material"; // Sustituye "Componente" por el nombre del componente que desees importar
+
+import { GetOneOrderByID } from "../../service/remote/get/GetOneOrderByID";
+import { OrdenesDetallesFValues } from "../../helpers/OrdenesDetallesFValues";
+import { UpdatePatchOneOrder } from "../../service/remote/post/AddOrdenesEstatus";
+import { GetAllLabels } from "../../../labels/services/remote/get/GetAllLabels";
 
 
 const OrdenesDetalleFModal = ({
-  showModal,
-  setShowModal
-  //handleReload
+  showModalF,
+  setShowModalF,
+  row,
+  index,
+  handleReload
   // Otros props que desees pasar al modal
 }) => {
   const [mensajeErrorAlert, setMensajeErrorAlert] = useState("");
   const [mensajeExitoAlert, setMensajeExitoAlert] = useState("");
   const [Loading, setLoading] = useState(false);
-
+  const [OrdenesValuesLabel, setOrdenesValuesLabel] = useState([]);
+  
   const formik = useFormik({
     initialValues: {
         IdTipoEstatusOK: "",
-        Actual: "S",
+        Actual: true,
         Observacion: ""
     },
     validationSchema: Yup.object({
         IdTipoEstatusOK: Yup.string().required("Campo requerido"),
-        Actual: Yup.string().required("Campo requerido").max(1, 'Solo se permite una letra').matches(/^[SN]$/, 'Solo se permite un caracter S/N'),
-        Observacion: Yup.string().required("Campo requerido"),
+        Actual: Yup.boolean().required("Campo requerido"),
     }),
     onSubmit: async (values) => {
       setMensajeExitoAlert("");
       setMensajeErrorAlert("");
       setLoading(true);
-
+       //FIC: reiniciamos los estados de las alertas de exito y error.
+       setMensajeErrorAlert(null);
+       setMensajeExitoAlert(null);
       try {
-        // Lógica para guardar la información en la base de datos
+       // Lógica para guardar la información en la base de datos
+        //console.log(row.IdInstitutoOK,row.IdNegocioOK,row.IdOrdenOK)
+        const ordenExistente = await GetOneOrderByID(row.IdInstitutoOK,row.IdNegocioOK,row.IdOrdenOK);
+
+        console.log("<<Ordenes>>",ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f);
+        
+        for (let i = 0; i < ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f.length; i++) {
+            console.log("Entro")
+            ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f[i]= {
+                IdTipoEstatusOK: ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f[i].IdTipoEstatusOK,
+                Actual: "N",
+                Observacion:ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f[i].Observacion
+              };
+              console.log("Realizo",ordenExistente)
+        }
+        
+        values.Actual == true ? (values.Actual = "S") : (values.Actual = "N");
+        //ordenExistente.ordenes_detalle[index].pedidos_detalle_ps_estatus_f.push(values);
+        //console.log(ordenExistente);
+
+        const EstatusOrdenes = OrdenesDetallesFValues(values, ordenExistente,index);
+        //const EstatusOrdenes = OrdenesEstatusValues(values);
+        
+        //console.log("<<Ordenes>>", EstatusOrdenes);
+        // console.log("LA ID QUE SE PASA COMO PARAMETRO ES:", row._id);
+        // Utiliza la función de actualización si estamos en modo de edición
+        
+        await UpdatePatchOneOrder(row.IdInstitutoOK,row.IdNegocioOK,row.IdOrdenOK,EstatusOrdenes);
         setMensajeExitoAlert("Envío actualizado correctamente");
-        //handleReload();
+        handleReload();
       } catch (e) {
+        setMensajeExitoAlert(null);
         setMensajeErrorAlert("No se pudo registrar");
       }
       setLoading(false);
@@ -63,10 +104,35 @@ const OrdenesDetalleFModal = ({
     disabled: !!mensajeExitoAlert,
   };
 
+  async function getDataSelectOrdenesType2() {
+    try {
+      const Labels = await GetAllLabels();
+      const OrdenesTypes = Labels.find(
+        (label) => label.IdEtiquetaOK === "IdTipoEstatusFisicoProdServ"
+      );
+      const valores = OrdenesTypes.valores; // Obtenemos el array de valores
+      const IdValoresOK = valores.map((valor, index) => ({
+        IdValorOK: valor.Valor,
+        key: valor.IdValorOK, // Asignar el índice como clave temporal
+      }));
+      setOrdenesValuesLabel(IdValoresOK);
+      console.log(OrdenesValuesLabel)
+    } catch (e) {
+      console.error(
+        "Error al obtener Etiquetas para Tipos Giros de Institutos:",
+        e
+      );
+    }
+  }
+
+  useEffect(() => {
+    getDataSelectOrdenesType2();
+  },[]);
+
   return (
-    <Dialog open={showModal}
-    onClose={() => setShowModal(false)}
-            fullWidth
+    <Dialog open={showModalF}
+    onClose={() => setShowModalF(false)}
+    fullWidth  
     >
       <form onSubmit={(e) => {formik.handleSubmit(e)}}>
         <DialogTitle>
@@ -74,40 +140,49 @@ const OrdenesDetalleFModal = ({
             <strong>Agregar Nuevo Estado de la Orden</strong>
           </Typography>
         </DialogTitle>
-        <DialogContent sx={{ display: "flex", flexDirection: "column" }}>
-          <TextField
-            id="IdTipoEstatusOK"
-            label="IdTipoEstatusOK*"
-            value={formik.values.IdTipoEstatusOK}
-            {...commonTextFieldProps}
-            error={
-              formik.touched.IdTipoEstatusOK &&
-              Boolean(formik.errors.IdTipoEstatusOK)
-            }
-            helperText={
-              formik.touched.IdTipoEstatusOK && formik.errors.IdTipoEstatusOK
-            }
+        <DialogContent sx={{ display: "flex", flexDirection: "column" }}
+        dividers
+        >
+       <InputLabel htmlFor="dynamic-select-tipo-orden">Estatus Físico del Producto/Servicio</InputLabel>
+          <Select
+              id="dynamic-select-tipo-orden"
+              value={formik.values.IdTipoEstatusOK}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              name="IdTipoEstatusOK"
+              aria-label="IdTipoEstatusOK"
+          >
+              {OrdenesValuesLabel.map((option, index) => (
+              <MenuItem key={option.IdValorOK} value={`IdTipoEstatusFisicoProdServ-${option.key}`}>
+                  {option.IdValorOK}
+              </MenuItem>
+              ))}
+          </Select>
+          <FormControlLabel
+              control={
+              <Checkbox
+                  id="Actual"
+                  checked={formik.values.Actual}  // Suponiendo que formik.values.Actual es un booleano
+                  onChange={(event) => {
+                  formik.setFieldValue('Actual', event.target.checked);
+                  }}
+                  disabled={!!mensajeExitoAlert}
+              />
+              }
+              label="Actual*"
+              error={formik.touched.Actual && Boolean(formik.errors.Actual)}
+              helperText={formik.touched.Actual && formik.errors.Actual}
           />
           <TextField
-            id="Actual"
-            label="Actual*"
-            value={formik.values.Actual}
-            {...commonTextFieldProps}
-            error={formik.touched.Actual && Boolean(formik.errors.Actual)}
-            helperText={formik.touched.Actual && formik.errors.Actual}
-          />
-          <TextField
-            id="Observacion"
-            label="Observacion*"
-            value={formik.values.Observacion}
-            {...commonTextFieldProps}
-            error={
-              formik.touched.Observacion &&
-              Boolean(formik.errors.Observacion)
-            }
-            helperText={
-              formik.touched.Observacion && formik.errors.Observacion
-            }
+              id="Observacion"
+              label="Observacion*"
+              multiline
+              rows={4}    
+              maxRows={10}
+              value={formik.values.Observacion}
+              {...commonTextFieldProps}
+              error={ formik.touched.Observacion && Boolean(formik.errors.Observacion) }
+              helperText={ formik.touched.Observacion && formik.errors.Observacion }
           />
           {/* Agregar otros campos aquí si es necesario */}
         </DialogContent>
@@ -129,7 +204,7 @@ const OrdenesDetalleFModal = ({
             loadingPosition="start"
             startIcon={<CloseIcon />}
             variant="outlined"
-            onClick={()=>setShowModal(false)}
+            onClick={() => setShowModalF(false)}
           >
             <span>CERRAR</span>
           </LoadingButton>
--- src/ecommerce/orders/components/tables/OrdenesDetalleFTable.jsx
+++ src/ecommerce/orders/components/tables/OrdenesDetalleFTable.jsx
@@ -1,16 +1,14 @@
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Dialog
 } from "@mui/material";
 import { MaterialReactTable } from "material-react-table";
 import { MRT_Localization_ES } from "material-react-table/locales/es";
-import React, { useEffect, useState } from "react";
 
-//import InfoIcon from "@mui/icons-material/Info";
-//import { Componente } from "@mui/material"; // Sustituye "Componente" por el nombre del componente que desees importar
-
-import BarActionsTable from "../../../../share/components/elements/bars/BarActionsTable";
 import OrdenesDetalleFModal from "../modals/OrdenesDetalleFModal";
+import BarActionsTable from "../../../../share/components/elements/bars/BarActionsTable";
+import { GetOneOrderByID } from "../../service/remote/get/GetOneOrderByID";
 
 
 import { useSelector } from "react-redux";
@@ -19,17 +17,19 @@ const OrdenesDetalleFTable = ({
 
 }) => {
   const [loadingTable, setLoadingTable] = useState(true);
-  const [OrdenesDetalleData, setOrdenesDetalleData] = useState([]);
+  const [OrdenesDetalleFData, setOrdenesDetalleFData] = useState([]);
   const [selectedRowIndex, setSelectedRowIndex] = useState(null); //Para saber cual es la fila y pasarla para el color de la tabla
 
-  const [showModal, setShowModal] = useState(false);
+  const [showModalF, setShowModalF] = useState(false);
     //Con redux sacar la data que se envió del otro archivo (ShippingsTable)
-  const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesDetalleData);
+  const selectedOrdenesData = useSelector((state) => state.ordenesReducer.selectedOrdenesData);
+  const index=useSelector((state) => state.ordenesReducer.index)
 
   useEffect(() => {
     async function fetchData() {
       try {
-        setOrdenesDetalleData(selectedOrdenesData.pedidos_detalle_ps_estatus_f); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
+        const OrdenesDetalledata = await GetOneOrderByID(selectedOrdenesData.IdInstitutoOK,selectedOrdenesData.IdNegocioOK,selectedOrdenesData.IdOrdenOK);
+        setOrdenesDetalleFData(OrdenesDetalledata.ordenes_detalle[index].pedidos_detalle_ps_estatus_f); //Se ponen los datos en el useState pero solo los del subdocumento info_ad
         setLoadingTable(false);
       } catch (error) {
         console.error("Error al obtener datos:", error);
@@ -39,12 +39,12 @@ const OrdenesDetalleFTable = ({
   }, []);
 
   const handleReload = async () => {
-    const OneOrdenesData = await GetOneOrderByID(selectedOrdenesData.IdInstitutoOK,selectedOrdenesData.IdNegocioOK,selectedOrdenesData.IdOrdenOK);
-    //setOrdenesEstatusData(OneOrdenesData.ordenes_estatus);
+    const OrdenesDetalledata = await GetOneOrderByID(selectedOrdenesData.IdInstitutoOK,selectedOrdenesData.IdNegocioOK,selectedOrdenesData.IdOrdenOK);
+    setOrdenesEstatusData(OrdenesDetalledata.ordenes_detalle[index].pedidos_detalle_ps_estatus_f);
     setSelectedRowIndex(null);
   };
 
-  const OrdenesDetalleColumn = [
+  const OrdenesDetalleFColumn = [
     {
         accessorKey: "IdTipoEstatusOK",
         header: "Id Tipo Estatus OK",
@@ -66,8 +66,8 @@ const OrdenesDetalleFTable = ({
     <Box>
       <Box>
       <MaterialReactTable
-        columns={OrdenesDetalleColumn}
-        data={OrdenesDetalleData}
+        columns={OrdenesDetalleFColumn}
+        data={OrdenesDetalleFData}
         state={{ isLoading: loadingTable }}
         initialState={{ density: "compact", showGlobalFilter: true }}
         enableColumnActions={false}
@@ -85,7 +85,7 @@ const OrdenesDetalleFTable = ({
         positionToolbarAlertBanner="bottom"
         renderTopToolbarCustomActions={({ table }) => (
             <BarActionsTable
-          handleBtnAdd={() => setShowModal(true)}
+          handleBtnAdd={() => setShowModalF(true)}
           handleBtnDetails={() => console.log("clic handleBtnDetails")}
           handleBtnReload={() => handleReload()}
         />
@@ -100,12 +100,13 @@ const OrdenesDetalleFTable = ({
     </Box>
 
       {/* Modal para la vista detallada */}
-      <Dialog open={showModal}>
+      <Dialog open={showModalF}>
         <OrdenesDetalleFModal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          //handleReload={handleReload}
-          onClose={() => setShowModal(false)}
+          showModalF={showModalF}
+          setShowModalF={setShowModalF}
+          row={selectedOrdenesData}
+          handleReload={handleReload}
+          onClose={() => setShowModalF(false)}
 
           // ...otros props necesarios
         />
